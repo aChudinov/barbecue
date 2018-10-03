60dc32816603ad09d589b84b67cd75cbd4e24ddc
Alexey Chudinov <alexey.chudinov@4finance.com>
2018-10-03 11:30:43 -0400

    Catch exception on fetch fail

--- src/prismic/fetch.js
+++ src/prismic/fetch.js
@@ -2,12 +2,16 @@ import apiEndpoint from './config';
 import transform from './transformer';
 import Prismic from 'prismic-javascript';
 
-export default async function getData() {
-  const api = await Prismic.getApi(apiEndpoint);
-  const response = await api.query('');
+export default async function fetchData() {
+  try {
+    const api = await Prismic.getApi(apiEndpoint) || { query: () => {} };
+    const response = await api.query('') || { results: [] };
 
-  return response.results.map(data => ({
-    id: data.id.toLowerCase(),
-    ...transform(data),
-  }));
+    return response.results.map(data => ({
+      id: data.id.toLowerCase(),
+      ...transform(data),
+    }));
+  } catch (error) {
+    return [];
+  }
 }
